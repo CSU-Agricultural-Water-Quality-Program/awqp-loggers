c6c0b8378d498c65e9363ea27d20d3a91a9e3fb9
mannydeleon99 <mannydeleon99@gmail.com>
2025-03-26 15:09:06 -0600

    Updated for 2 sth31

--- awqp-logger-sht31-v1/src/awqp-logger-sht31-v1.ino
+++ awqp-logger-sht31-v1/src/awqp-logger-sht31-v1.ino
@@ -42,204 +42,178 @@ firmwares to accomodate.
 
 // Libs
 
-//#include <Adafruit_GPS.h>
+PRODUCT_VERSION(18);
+
 #include <adafruit-sht31.h>
 #include <RunningAverage.h>
+#include <Wire.h>
 
-// Definitions
-#define subsamples 5 // # of readings to average for SHT3X sensor output
+#define TCAADDR 0x70
+#define subsamples 5
 #define DELAY_BEFORE_REBOOT 2000
 
-
-// initialize sht31 T and RH sensor
+// Initialize sensors and mux
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
-  // Configure temp and RH vars
-float temp;
-float hum;
+float temp1, hum1;
+float temp2, hum2;
+RunningAverage raT1(subsamples);
+RunningAverage raH1(subsamples);
+RunningAverage raT2(subsamples);
+RunningAverage raH2(subsamples);
 
-// Configure running average variables
-RunningAverage myRA1(subsamples);
-RunningAverage myRA2(subsamples);
-
-// Boron diagnostics
+// Diagnostics
 FuelGauge fuel;
 float batteryVoltage;
 int stateOfCharge;
-int rat;
 float strengthPercentage = 0.0;
 float qualityPercentage = 0.0;
+int rat;
 
-// Timers for loop()
+// Timer
 int Time_old;
 
-// Remote system reset variables
+// Remote triggers
 unsigned int rebootDelayMillis = DELAY_BEFORE_REBOOT;
 unsigned long rebootSync = millis();
 bool resetFlag = false;
-
-// Remote takeMeasurementsNow variable
 bool resetFlagReadings = false;
 
-// Variables Related To Particle Mobile Application Reporting and Webhook
+// Cloud Variables
 char SignalString[64];
 char batteryString[16];
-char tempString[16];   // Temp Sensor in Celcius
-char humString[16];   // Relative Humidity %
-char positionString[64];    // GPS location in lat/long decimal degrees
+char tempString[32];
+char humString[32];
+char positionString[64];
 
-// Sleep function: uncomment to add
 SystemSleepConfiguration config;
-
-// Power Configuration for Solar Charging
 SystemPowerConfiguration conf;
 
+void tcaSelect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
+
 void setup() {
-    // Serial print & publish program device info upon setup initiation
-    Serial.begin(9600);
-    Particle.publish("AWQP Sensor Initiated: SHT3X, "+Time.timeStr());
-    Particle.publish("Settings: Readings; 30 min, Sleep; Yes");
-    Serial.println(">>> AWQP Sensor Initiated: SHT3X, "+Time.timeStr());
-    Serial.println(">>> Settings: Readings; 30 min, Sleep; Yes");
+  Serial.begin(9600);
+  Particle.publish("AWQP Sensor Initiated: Dual SHT3X, "+Time.timeStr());
 
-    // Sensor Setup
-     // turn on Sensors
-    Wire.begin();
-    sht31.begin(0x44);
-    myRA1.clear();
-    myRA2.clear();
-     // Particle variables that enable monitoring via mobile app
-    Particle.variable("BattVolt", batteryString);
-    Particle.variable("SensorT", tempString);
-    Particle.variable("SensorRH", humString);
-    Particle.variable("Signal", SignalString);
-    Particle.variable("Position", positionString);
-        
-    //  Remote Reset Function Setup
-    Particle.function("reset", cloudResetFunction);
-    
-    //  Take Measurements Function Setup
-    Particle.function("TakeMeasurementsNow", takeMeasurementNow);
+  Wire.begin();
+  tcaSelect(0);
+  sht31.begin(0x44);
+  tcaSelect(1);
+  sht31.begin(0x44);
+  raT1.clear(); raH1.clear();
+  raT2.clear(); raH2.clear();
 
-    // Sleep Setup: uncomment to add
-    config.mode(SystemSleepMode::STOP)
-          .duration(27min);   // set sleep duration in minutes here, wake up 3 minutes before readings
-          
-    //  set D7 pin to OUTPUT for LED blink upon measurement
-    pinMode(D7, OUTPUT);
+  Particle.variable("BattVolt", batteryString);
+  Particle.variable("SensorT", tempString);
+  Particle.variable("SensorRH", humString);
+  Particle.variable("Signal", SignalString);
+  Particle.variable("Position", positionString);
+  Particle.function("reset", cloudResetFunction);
+  Particle.function("TakeMeasurementsNow", takeMeasurementNow);
 
+  config.mode(SystemSleepMode::STOP).duration(27min);
+  pinMode(D7, OUTPUT);
 }
 
 void loop(){
-    
-	// Collect sensor readings and send payload
-        // Samples every 2 hr. change the "2" to change sample interval in hours (0 - 23)
-    if(Time.minute() % 30 == 0 && Time_old != Time.minute()){  
-        getSignalStrength(); // signal diagnostics
-        takeMeasurements(); // read temperatures
-        sendData(); // send payload
-        digitalWrite(D7, HIGH); // turn on LED
-        Time_old = Time.minute(); // resetting timer
-        delay(8000); // needed to allow time for data to send prior to sleep initiation
-        digitalWrite(D7, LOW); // turn off LED
-        System.sleep(config); // set device to sleep
-    }
-    
-    //  Remote Reset Function
-    if ((resetFlag) && (millis() - rebootSync >=  rebootDelayMillis)) {
-        // do things here before reset if necessary and then push the button
-        Particle.publish("Debug", "Remote Reset Initiated", 300, PRIVATE);
-        System.reset();
-    }    
-    //  Take Measurements Now Function
-    if (resetFlagReadings) {
-        Particle.publish("Input Recieved: Taking Measurements. "+Time.timeStr());
-        getSignalStrength(); // signal diagnostics
-        takeMeasurements(); // read temperatures
-        sendData(); // send payload
-        digitalWrite(D7, HIGH); // turn on LED
-        Particle.publish("Measurements taken and payload sent. "+Time.timeStr());
-        delay(3000); // needed to allow time for data to send
-        resetFlagReadings = false; // reset function to use again
-        digitalWrite(D7, LOW); // turn off LED
-    }
-}
-
-void sendData() { 
-    // send data to particle cloud
-        // defining what timestamp_seconds is
-    unsigned long timestamp_seconds = Time.now(); 
-    // create a message with 512 charaters. 
-        // note that says this needs be declared locally (in fxn), not globally --AJB
-    char msg[512];  
-    // to publish data to cloud it needs to be a string format.
-              //note: removing variables from msg still sends to ubidots, ie it's okay to not have new position variable; it will still send other variables
-    snprintf(msg, sizeof(msg) - 1, "{\"T\":%4.1f, \"RH\":%4.1f, \"battery\":%4.1f, \"charge\":%i, \"Signal\":%4.1f, \"timestamp\":%lu000}", temp, hum, batteryVoltage, stateOfCharge, strengthPercentage, timestamp_seconds); //to publish data to cloud it needs to be a string format.
-    
-    //send data to cloud
-    Particle.publish("AWQP_Cercospora", msg, PRIVATE); // Send payload to AWQP Ubidots
-    Particle.publish("AWQP_Sensor_SHT", msg, PRIVATE); // Send payload to WS IoT Hub
-    Serial.println("Particle Published!");
-    //Serial.println(msg);
+  if(Time.minute() % 30 == 0 && Time_old != Time.minute()){  
+    getSignalStrength();
+    takeMeasurements();
+    sendData();
+    digitalWrite(D7, HIGH);
+    Time_old = Time.minute();
+    delay(8000);
+    digitalWrite(D7, LOW);
+    System.sleep(config);
   }
 
+  if ((resetFlag) && (millis() - rebootSync >=  rebootDelayMillis)) {
+    Particle.publish("Debug", "Remote Reset Initiated", 300, PRIVATE);
+    System.reset();
+  }    
+
+  if (resetFlagReadings) {
+    Particle.publish("Input Recieved: Taking Measurements. "+Time.timeStr());
+    getSignalStrength();
+    takeMeasurements();
+    sendData();
+    digitalWrite(D7, HIGH);
+    Particle.publish("Measurements taken and payload sent. "+Time.timeStr());
+    delay(3000);
+    resetFlagReadings = false;
+    digitalWrite(D7, LOW);
+  }
+}
+
+void sendData() {
+  unsigned long timestamp_seconds = Time.now(); 
+  char msg[512];
+  snprintf(msg, sizeof(msg) - 1,
+    "{\"T1\":%4.1f, \"RH1\":%4.1f, \"T2\":%4.1f, \"RH2\":%4.1f, \"battery\":%4.1f, \"charge\":%i, \"Signal\":%4.1f, \"timestamp\":%lu000}",
+    temp1, hum1, temp2, hum2, batteryVoltage, stateOfCharge, strengthPercentage, timestamp_seconds);
+
+  Particle.publish("AWQP_Cercospora", msg, PRIVATE);
+  Particle.publish("AWQP_Sensor_SHT", msg, PRIVATE);
+  Serial.println("Particle Published!");
+}
+
 bool takeMeasurements() {
-
   if (Cellular.ready()){
-  // Get battery voltage level and state  of charge
-  batteryVoltage = fuel.getVCell();  // = 4.0 Voltage level of battery
-  snprintf(batteryString, sizeof(batteryString), "%4.1f V", batteryVoltage);
-  stateOfCharge = int(fuel.getSoC());
-  
-  // take 30ms readings and average for robust data
-    for (byte i=0; i<=subsamples; i++)   { 
-      temp = sht31.readTemperature();
-      hum = sht31.readHumidity();
+    batteryVoltage = fuel.getVCell();
+    snprintf(batteryString, sizeof(batteryString), "%4.1f V", batteryVoltage);
+    stateOfCharge = int(fuel.getSoC());
+
+    for (byte i=0; i<=subsamples; i++) {
+      tcaSelect(0);
+      raT1.addValue(sht31.readTemperature());
+      raH1.addValue(sht31.readHumidity());
+      tcaSelect(1);
+      raT2.addValue(sht31.readTemperature());
+      raH2.addValue(sht31.readHumidity());
       delay(30);
-      myRA1.addValue(temp);
-      myRA2.addValue(hum);
     }
-    temp=myRA1.getAverage(); // temp in Celcius (average of 5 readings)
-    hum=myRA2.getAverage(); // rh in % (average of 5 readings)
 
-  // convert to string characters
-    snprintf(tempString,sizeof(tempString), "%4.1f C", temp);
-    snprintf(humString,sizeof(humString), "%4.1f%%", hum);
-  
-  // reset running average vars
-    myRA1.clear();
-    myRA2.clear();
+    temp1 = raT1.getAverage();
+    hum1 = raH1.getAverage();
+    temp2 = raT2.getAverage();
+    hum2 = raH2.getAverage();
 
+    snprintf(tempString,sizeof(tempString), "T1: %4.1f C, T2: %4.1f C", temp1, temp2);
+    snprintf(humString,sizeof(humString), "RH1: %4.1f%%, RH2: %4.1f%%", hum1, hum2);
+
+    raT1.clear(); raH1.clear();
+    raT2.clear(); raH2.clear();
   }
   return 1;
 }
 
 void getSignalStrength() {
-    CellularSignal sig = Cellular.RSSI();
-    rat = sig.getAccessTechnology();
-    strengthPercentage = sig.getStrength();
-    qualityPercentage = sig.getQuality();
-    snprintf(SignalString,sizeof(SignalString), "S:%2.0f%%, Q:%2.0f%% ", strengthPercentage, qualityPercentage);
+  CellularSignal sig = Cellular.RSSI();
+  rat = sig.getAccessTechnology();
+  strengthPercentage = sig.getStrength();
+  qualityPercentage = sig.getQuality();
+  snprintf(SignalString,sizeof(SignalString), "S:%2.0f%%, Q:%2.0f%% ", strengthPercentage, qualityPercentage);
 }
 
-int cloudResetFunction(String command) {//  Remote Reset Function
-    resetFlag = true;
-    rebootSync = millis();
-    return 1;
-    // You would call the function by typing “true” in the particle console
+int cloudResetFunction(String command) {
+  resetFlag = true;
+  rebootSync = millis();
+  return 1;
 }
 
 int takeMeasurementNow(String command) {
-    resetFlagReadings = true;
-    return 1;
+  resetFlagReadings = true;
+  return 1;
 }
 
 void batt_settings() {
-    // Optimized for smaller 2500 mAh LiPo battery: https://acrobat.adobe.com/id/urn:aaid:sc:US:15adc4fb-8183-4845-b129-5039b2164b1e
-    conf.powerSourceMaxCurrent(1500) // for 2500 mAh bat., this is 1 C5A or 1.0*2500 = 2500 mA; default is 900 mA; due to boron components, real max is ~1500
-    .powerSourceMinVoltage(3750) // Set minimum voltage required for VIN to be used = 3.75V; default is 3880
-    .batteryChargeCurrent(500) // for 2500 mAh bat., this is 0.2 C5A or 0.2*2500 = 500 mA; default is 896 mA
-    .batteryChargeVoltage(4200); // set at 4.2V for 2500 mAh bat.; default is 4.112V termination voltage
-    //.feature(SystemPowerFeature::USE_VIN_SETTINGS_WITH_USB_HOST);
-    System.setPowerConfiguration(conf); // retains these settings
+  conf.powerSourceMaxCurrent(1500)
+      .powerSourceMinVoltage(3750)
+      .batteryChargeCurrent(500)
+      .batteryChargeVoltage(4200);
+  System.setPowerConfiguration(conf);
 }
-
